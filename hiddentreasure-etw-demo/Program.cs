6c327d77a69fce5e2b8d49c6c1a4b81bff9746ab
Zac Brown (ODSP SECURITY) <zbrown@microsoft.com>
2017-01-19 15:43:07 -0800

    Minimal cleaned up demo.
    Signed-off-by: Zac Brown (ODSP SECURITY) <zbrown@microsoft.com>

--- hiddentreasure-etw-demo/Program.cs
+++ hiddentreasure-etw-demo/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using O365.Security.ETW;
-using System.Threading;
 
 namespace hiddentreasure_etw_demo
 {
@@ -8,60 +7,22 @@ namespace hiddentreasure_etw_demo
     {
         static void Main(string[] args)
         {
-            var trace = new UserTrace();
+            var filter = new EventFilter(Filter
+                .EventIdIs(3018)
+                .Or(Filter.EventIdIs(3020)));
 
-            // The name of the PowerShell provider that gives us detailed
-            // method execution logging is "Microsoft-Windows-PowerShell".
-            var powershellProvider = new Provider("Microsoft-Windows-PowerShell");
-
-            var powershellFilter = new EventFilter(Filter
-                .EventIdIs(7937)
-                .And(UnicodeString.Contains("Payload", "Started")));
-
-            powershellFilter.OnEvent += OnEvent;
-
-            // The "Any" and "All" flags vary from Provider to Provider.
-            // You'll have to play with them in Microsoft Message Analyzer
-            // or a similar tool to figure out what flags you need.
-            powershellProvider.Any = 0x20;
-            powershellProvider.AddFilter(powershellFilter);
-
-            trace.Enable(powershellProvider);
-
-            Console.CancelKeyPress += (sender, eventArg) =>
-            {
-                if (trace != null)
-                {
-                    Console.WriteLine("stopping ETW trace...");
-                    Thread.Sleep(1000);
-                    trace.Stop();
-                }
+            filter.OnEvent += (IEventRecord r) => {
+                var query = r.GetUnicodeString("QueryName");
+                var result = r.GetUnicodeString("QueryResults");
+                Console.WriteLine($"DNS query ({r.Id}): {query} - {result}");
             };
 
-            Console.WriteLine("starting ETW trace...");
+            var provider = new Provider("Microsoft-Windows-DNS-Client");
+            provider.AddFilter(filter);
 
-            // This is a blocking call. Ctrl-C to stop.
+            var trace = new UserTrace();
+            trace.Enable(provider);
             trace.Start();
-        }
-
-        static void OnEvent(IEventRecord record)
-        {
-            string data = string.Empty;
-
-            // The event property which contains the interesting information
-            // in the PowerShell method invocation events is called "ContextInfo".
-            // It's essentialy a giant well-formatted string blob.
-            if (!record.TryGetUnicodeString("ContextInfo", out data))
-            {
-                Console.WriteLine("Could not parse 'ContextInfo' from PowerShell event");
-                return;
-            }
-
-            if (data.IndexOf("invoke-mimikatz", StringComparison.OrdinalIgnoreCase) >= 0)
-            {
-                Console.WriteLine($"BAD METHOD!");
-                Console.WriteLine(data);
-            }
         }
     }
 }
